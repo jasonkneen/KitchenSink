bb6dcbde2c44385919bac996b773c05e61cbf958
vishalduggal <vduggal@appcelerator.com>
2014-12-17 08:14:07 -0800

    [TIMOB-18238] Camera Fixes

--- Resources/ui/handheld/ios/phone/camera_file.js
+++ Resources/ui/handheld/ios/phone/camera_file.js
@@ -3,45 +3,50 @@ function cam_file(_args) {
 		title:_args.title
 	});
 	
-	Titanium.Media.showCamera({
-	
-		success:function(event)
-		{
-			var cropRect = event.cropRect;
-			var image = event.media;
-			var filename = Titanium.Filesystem.applicationDataDirectory + "/"+ 'camera_photo' + new Date().getTime() + ".png";
-			var f = Titanium.Filesystem.getFile(filename);
-			if (f.exists()) {
-				Ti.API.info('The file exist , trying to delete it before using it :' + f.deleteFile());
-				f = Titanium.Filesystem.getFile(filename);
-			}
-			f.write(image);
-			win.backgroundImage = f.nativePath;
-		},
-		cancel:function()
-		{
-	
-		},
-		error:function(error)
-		{
-			// create alert
-			var a = Titanium.UI.createAlertDialog({title:'Camera'});
-	
-			// set message
-			if (error.code == Titanium.Media.NO_CAMERA)
+	var showCamera = function(){
+		Titanium.Media.showCamera({
+		
+			success:function(event)
 			{
-				a.setMessage('Device does not have video recording capabilities');
-			}
-			else
+				var cropRect = event.cropRect;
+				var image = event.media;
+				var filename = Titanium.Filesystem.applicationDataDirectory + "/"+ 'camera_photo' + new Date().getTime() + ".png";
+				var f = Titanium.Filesystem.getFile(filename);
+				if (f.exists()) {
+					Ti.API.info('The file exist , trying to delete it before using it :' + f.deleteFile());
+					f = Titanium.Filesystem.getFile(filename);
+				}
+				f.write(image);
+				win.backgroundImage = f.nativePath;
+			},
+			cancel:function()
 			{
-				a.setMessage('Unexpected error: ' + error.code);
-			}
+		
+			},
+			error:function(error)
+			{
+				// create alert
+				var a = Titanium.UI.createAlertDialog({title:'Camera'});
+		
+				// set message
+				if (error.code == Titanium.Media.NO_CAMERA)
+				{
+					a.setMessage('Device does not have video recording capabilities');
+				}
+				else
+				{
+					a.setMessage('Unexpected error: ' + error.code);
+				}
+		
+				// show alert
+				a.show();
+			},
+			allowEditing:true
+		});
+	};
 	
-			// show alert
-			a.show();
-		},
-		allowEditing:true
-	});
+	win.addEventListener('open',showCamera);
+	
 	return win;
 };
 
--- Resources/ui/handheld/ios/phone/camera_gallery.js
+++ Resources/ui/handheld/ios/phone/camera_gallery.js
@@ -3,38 +3,41 @@ function cam_gallery(_args) {
 		title:_args.title
 	});
 	
-	
-	Titanium.Media.showCamera({
-	
-		success:function(event)
-		{
-			Titanium.UI.createAlertDialog({title:'Camera', message:'Check your Photo Gallery'}).show();
-		},
-		cancel:function()
-		{
-	
-		},
-		error:function(error)
-		{
-			// create alert
-			var a = Titanium.UI.createAlertDialog({title:'Camera'});
-	
-			// set message
-			if (error.code == Titanium.Media.NO_CAMERA)
+	var showCamera = function(){
+		Titanium.Media.showCamera({
+		
+			success:function(event)
 			{
-				a.setMessage('Device does not have video recording capabilities');
-			}
-			else
+				Titanium.UI.createAlertDialog({title:'Camera', message:'Check your Photo Gallery'}).show();
+			},
+			cancel:function()
 			{
-				a.setMessage('Unexpected error: ' + error.code);
-			}
+		
+			},
+			error:function(error)
+			{
+				// create alert
+				var a = Titanium.UI.createAlertDialog({title:'Camera'});
+		
+				// set message
+				if (error.code == Titanium.Media.NO_CAMERA)
+				{
+					a.setMessage('Device does not have video recording capabilities');
+				}
+				else
+				{
+					a.setMessage('Unexpected error: ' + error.code);
+				}
+		
+				// show alert
+				a.show();
+			},
+			saveToPhotoGallery:true,
+			allowEditing:true
+		});		
+	};
 	
-			// show alert
-			a.show();
-		},
-		saveToPhotoGallery:true,
-		allowEditing:true
-	});
+	win.addEventListener('open',showCamera);
 	return win;
 };
 
